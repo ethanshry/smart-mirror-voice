76fda66acb2a4593c275d66e2e79c50215502711
Ethan Shry <ehshry@gmail.com>
2018-02-11 16:58:32 -0600

    basic GPIO code

--- HardwareInterface/voiceRecognizer.py
+++ HardwareInterface/voiceRecognizer.py
@@ -4,9 +4,20 @@ import asyncio
 import json
 import requests
 
+import RPi.GPIO as GPIO
+
+
 from websocket import create_connection
 
+config = {
+    "shouldSpeak": False,
+    "shouldTriggerVisualIndicator": False
+}
+
 def main():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(26, GPIO.OUT)
+    global config
     recognizer = aiy.cloudspeech.get_recognizer()
     #recognizer.expect_phrase('turn off the light')
     recognizer.expect_hotword('Clara')
@@ -19,10 +30,12 @@ def main():
             print('Sorry, I did not hear you.')
         else:
             print('You said "', text, '"')
-            aiy.audio.say("One moment")
+            GPIO.output(26, GPIO.HIGH)
+            if config['shouldSpeak']: aiy.audio.say("One moment")
             ws = create_connection("ws://localhost:8080/websocket")
             ws.send(text)
             ws.close()
+            GPIO.output(26, GPIOO.LOW)
 
 if __name__ == '__main__':
     main()

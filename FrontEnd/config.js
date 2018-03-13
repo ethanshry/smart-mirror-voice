518c3aa515850b0cd917ed373e9cd970258fe2c0
Ethan Shry <ehshry@gmail.com>
2018-03-13 00:56:32 -0500

    config.js tokens removed and reupload

---
+++ FrontEnd/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    guiServerPort: 3000,
+    websocketServerPort: 8080,
+    shouldGiveVerbalResponse: false,
+    setCommandTimeout: undefined,
+    APIKeys: {
+        openweathermap: '',
+        twitter: {
+            consumerKey : '',
+            consumerSecret: '',
+            accessToken: '',
+            accessTokenSecret: ''
+        }
+    },
+    APIStrings: {
+        openweathermap: ''
+    }
+}

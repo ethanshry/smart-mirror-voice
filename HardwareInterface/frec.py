84d6a0a2c0f1bc4fb4510b0368269b4bb84275cd
ethanshry <ehshry@gmail.com>
2018-04-16 01:56:50 +0500

    frec working! woot!

--- HardwareInterface/frec.py
+++ HardwareInterface/frec.py
@@ -17,71 +17,75 @@ cam.hflip = True
 cam.capture('unknownFace.jpg')
 
 BUCKET = "tech-reflect-voice-facial-images"
-FACE_LIST = ["Ethan", "Anthony"]
+FACE_LIST = ["Ethan","Anthony"]
 KEY_TARGET = "unknownFace.jpg"
 RECOGNIZED_FACE = False
 IS_FACE = False
 MIN_SIM = 60.0
 
 s3 = boto3.client('s3')
-s3.upload_file('unknownFace.jpg', Bucket=BUCKET, Key=KEY_TARGET)
+#s3 = boto3.client(
+ #   's3',
+  #  aws_access_key_id='AKIAJU3D5B2SKBI6PXXQ',
+   # aws_secret_access_key='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',
+    #aws_session_token=SESSION_TOKEN,
+#)
+s3.upload_file('unknownFace.jpg', Bucket=BUCKET, Key = KEY_TARGET)
 
-
-def detect_face(bucket, key, region="us-east-2", attributes=['ALL']):
-        rekognition = boto3.client("rekognition", region)
-        response = rekognition.detect_faces(
+def detect_face(bucket, key, region = "us-east-2", attributes = ['ALL']):
+    rekognition = boto3.client("rekognition", region)
+    response = rekognition.detect_faces(
         Image={
-                "S3Object": {
+             "S3Object": {
                 "Bucket": bucket,
                 "Name": key
-                }
+             }
         },
-        Attributes=attributes,
-        )
-        return response['FaceDetails']
-
+        Attributes = attributes,
+    )
+    return response['FaceDetails']
 
 def compare_faces(bucket, key, bucket_target, key_target, threshold=0, region="us-east-2"):
-        rekognition = boto3.client("rekognition", region)
-        response = rekognition.compare_faces(
-        SourceImage={
-                "S3Object": {
-                "Bucket": bucket,
-                "Name": key,
-                }
-        },
-        TargetImage={
-                "S3Object": {
-                "Bucket": bucket_target,
-                "Name": key_target,
-                }
-        },
-        SimilarityThreshold=threshold,
-        )
-        return response['SourceImageFace'], response['FaceMatches']
+    rekognition = boto3.client("rekognition", region)
+    response = rekognition.compare_faces(
+    SourceImage={
+     "S3Object": {
+        "Bucket": bucket,
+        "Name": key,
+     }
+    },
+    TargetImage={
+     "S3Object": {
+        "Bucket": bucket_target,
+        "Name": key_target,
+     }
+    },
+    SimilarityThreshold=threshold,
+    )
+    return response['SourceImageFace'], response['FaceMatches']
 
 for face in detect_face(BUCKET, KEY_TARGET):
-        conf = face['Confidence']
-        if(conf >= MIN_SIM):
-                print("Face detected")
-                IS_FACE = True
-
-
-if(IS_FACE == True):
-        for face in FACE_LIST:
-                FILENAME = face + "Face.jpg"
-                source_face, matches = compare_faces(BUCKET, FILENAME, BUCKET, KEY_TARGET)
-                for match in matches:
-                        sim = match['Similarity']
-                        print(sim)
-                        if (sim >= MIN_SIM):
-                                print("User is " + face)
-                                RECOGNIZED_FACE = True
+    conf = face['Confidence']
+    if(conf >= MIN_SIM):
+        print "Face detected"
+        IS_FACE = True
+        
+        
+if(IS_FACE == True) :
+    for face in FACE_LIST:
+       FILENAME = face + "Face.jpg"
+       source_face, matches = compare_faces(BUCKET, FILENAME, BUCKET, KEY_TARGET)
+       for match in matches:
+          sim = match['Similarity']
+          print sim
+          if (sim >= MIN_SIM) :
+             print("User is " + face)
+             RECOGNIZED_FACE = True
 
 if (RECOGNIZED_FACE == False):
-        print("This face is not in the current registry")
+   print("This face is not in the current registry")
 else:
-        print("No face detected")
+    print("No face detected")
 
 # the main source face
 #print "Source Face ({Confidence}%)".format(**source_face)
@@ -93,3 +97,4 @@ else:
 
 #sim = match['Similarity']
 #print sim
+

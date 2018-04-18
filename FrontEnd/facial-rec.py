ae56511de6c762af851b8dcef849c2491831063f
ethanshry <ehshry@gmail.com>
2018-04-18 00:58:53 -0500

    string casts

--- FrontEnd/facial-rec.py
+++ FrontEnd/facial-rec.py
@@ -2,12 +2,8 @@
 import boto3
 import picamera as picam
 import sys
-
-#ACCESS KEY:
-#AKIAJXCO4MX5WMIJKU6Q
-#SECRET KEY:
-#/JOZY765qv02xVM4BRS+wnbfoQbx654m+I1hVyF4r
-
+import time
+#holds final message to be send to js
 message = []
 
 cam = picam.PiCamera()
@@ -26,12 +22,6 @@ IS_FACE = False
 MIN_SIM = 60.0
 
 s3 = boto3.client('s3')
-#s3 = boto3.client(
- #   's3',
-  #  aws_access_key_id='AKIAJU3D5B2SKBI6PXXQ',
-   # aws_secret_access_key='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',
-    #aws_session_token=SESSION_TOKEN,
-#)
 s3.upload_file('unknownFace.jpg', Bucket=BUCKET, Key = KEY_TARGET)
 
 def detect_face(bucket, key, region = "us-east-2", attributes = ['ALL']):
@@ -79,21 +69,19 @@ if(IS_FACE == True) :
        source_face, matches = compare_faces(BUCKET, FILENAME, BUCKET, KEY_TARGET)
        for match in matches:
           sim = match['Similarity']
-          #print(sim)
-          message.append("simScore:" + sim)
+          message.append("simScore:" + str(sim))
           if (sim >= MIN_SIM) :
-             message.append("user:" + face)
-             #print("User is " + face)
+             message.append("user:" + str(face))
              RECOGNIZED_FACE = True
 
 if (RECOGNIZED_FACE == False):
-   #print("This face is not in the current registry")
    message.append("user:undefined")
 else:
-    #print("No face detected")
     message.append("faceDetected:false")
 
 print(str.join(',', message))
+
+
 
 # the main source face
 #print "Source Face ({Confidence}%)".format(**source_face)
